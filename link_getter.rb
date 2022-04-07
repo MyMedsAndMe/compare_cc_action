b31d1e450a54579f2f6a8285d1f57b419762980d
Diana Álvarez <dianaalvarezco@gmail.com>
2022-04-07 09:51:59 +0100

    Debug

--- link_getter.rb
+++ link_getter.rb
@@ -19,25 +19,25 @@ end
 puts "This is the running script"
 puts "This is the experimental arg: #{ARGV[6]}"
 # Gets previous version of the modified file
-if ARGV[4] == true
+if ARGV[4]
   current_file = "#{ARGV[4]}"
   puts "This is the current file: #{current_file}"
-elsif ARGV[5] == true
+elsif ARGV[5]
   current_file = "#{ARGV[5]}"
-elsif ARGV[6] == true
+elsif ARGV[6]
   current_file = "#{ARGV[6]}"
   puts "This is the current file: #{current_file}"
-elsif ARGV[7] == true
+elsif ARGV[7]
   current_file = "#{ARGV[7]}"
-elsif ARGV[8] == true
+elsif ARGV[8]
   current_file = "#{ARGV[8]}"
 else
   puts "Nothing to do here"
 end
-before = YAML.load `git show #{ARGV[0]}:#{current_file}`
-# Gets updated version of the modified file
-after = YAML.load `git show #{ARGV[1]}:#{current_file}`
+# before = YAML.load `git show #{ARGV[0]}:#{current_file}`
+# # Gets updated version of the modified file
+# after = YAML.load `git show #{ARGV[1]}:#{current_file}`
 
-body_text = "Please, visit for checking the changes in #{current_file}: <a>https://github.com/MyMedsAndMe/customer_configurations/compare/#{find_version(before)}...#{find_version(after)}?expand=1</a>"
-# Run github cli command to add a comment to the PR
-puts `gh pr comment "#{ARGV[3]}" --body "#{body_text}"`
+# body_text = "Please, visit for checking the changes in #{current_file}: <a>https://github.com/MyMedsAndMe/customer_configurations/compare/#{find_version(before)}...#{find_version(after)}?expand=1</a>"
+# # Run github cli command to add a comment to the PR
+# puts `gh pr comment "#{ARGV[3]}" --body "#{body_text}"`
